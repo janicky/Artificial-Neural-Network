5e213d6d9e29ba22ab26cc805f300044d1b3411c
Patryk Janicki <kontakt.janicki@gmail.com>
2018-05-09 12:47:01 +0200

    Add getWeightsMatrix() method to `Configurator`

--- src/main/java/Configurator.java
+++ src/main/java/Configurator.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Configurator {
@@ -19,5 +20,21 @@ public class Configurator {
 
     public int getLayersCount() {
         return layers.size();
+    }
+
+    public double[][][] getWeightsMatrix(int input_count) {
+        double[][][] matrix = new double[getLayersCount()][][];
+        for(int i = 0; i < getLayersCount(); i++) {
+            matrix[i] = new double[layers.get(i)][];
+            for (int j = 0; j < layers.get(i); j++) {
+                matrix[i][j] = new double[input_count];
+            }
+            input_count = layers.get(i);
+        }
+        return matrix;
+    }
+
+    public List<Integer> getLayers() {
+        return Collections.unmodifiableList(layers);
     }
 }
