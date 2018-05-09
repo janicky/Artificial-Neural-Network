4458d9b6a5893fbdc267dab58cac7198a98ccf62
Patryk Janicki <kontakt.janicki@gmail.com>
2018-05-09 13:32:28 +0200

    Add some methods to `Layer`

--- src/main/java/Layer.java
+++ src/main/java/Layer.java
@@ -1,11 +1,45 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Layer {
 
-    private static int index = 1;
+    private static int index = 0;
     private int id;
-    private int neurons;
+    private List<Neurone> neurons = new ArrayList<Neurone>();
 
-    public Layer(int neurons) {
+    public Layer() {
         this.id = Layer.index++;
-        this.neurons = neurons;
+    }
+
+    public Layer(List<Neurone> neurons) {
+        this.id = Layer.index++;
+        this.neurons.addAll(neurons);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public static int getIndex() {
+        return index;
+    }
+
+    public static void resetIndex() {
+        index = 0;
+    }
+
+    public List<Neurone> getNeurons() {
+        return Collections.unmodifiableList(neurons);
+    }
+
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Layer: " + Integer.toString(id) + "\n");
+        sb.append("---------------------------------------\n");
+        for (Neurone neurone : neurons) {
+            sb.append(neurone.toString() + "\n");
+        }
+        return sb.toString();
     }
 }
